38ce9685f00b4a2558812caff79aebf73fe88cf4
Gene Soudlenkov <g.soudlenkov@.auckland.ac.nz>
2018-11-30 16:19:10 +1300

    Fix 21

--- library/library.cpp
+++ library/library.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "library.h"
 
+#ifndef _WINDOWS
 // Initializer.
 __attribute__((constructor))
 static void initializer(void) {
@@ -12,7 +13,7 @@ __attribute__((destructor))
 static void finalizer(void) {
     printf("[%s] finalizer()\n", __FILE__);
 }
-
+#endif
 
 int func1(int a)
 {
